91ed5051d0b4e1fc9b8fee7b25e92d53c0560418
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 14:04:55 -0600

    Comment out toss()

--- Coin Toss.py
+++ Coin Toss.py
@@ -9,17 +9,29 @@ def main():
     )  # this creates an instance called 'my_coin' of the class 'Coin()'
 
     # Display the side of the coin that is facing up.
-    print(
-        "This side is up:", my_coin.get_sideup()
-    )  # notice you do not have to supply the argument/parameter
+    # print(
+    #     "This side is up:", my_coin.get_sideup()
+    # )  # notice you do not have to supply the argument/parameter
 
-    # Toss the coin.
-    print("I am going to toss the coin ten times:")
-    for count in range(10):
-        my_coin.toss()
+    # # Toss the coin.
+    # print("I am going to toss the coin ten times:")
+    # for count in range(10):
+    #     my_coin.toss()
 
-        # Display the side of the coin that is facing up.
-        print("This side is up:", my_coin.get_sideup())
+    #     # Display the side of the coin that is facing up.
+    #     print("This side is up:", my_coin.get_sideup())
+
+    show_coin_status(my_coin)
+    flip(my_coin)
+    show_coin_status(my_coin)
+
+
+def show_coin_status(coin_obj):
+    print("This side of the coin is up:", coin_obj.get_sideup())
+
+
+def flip(coin_obj):
+    coin_obj.toss()
 
 
 # Call the main function.
