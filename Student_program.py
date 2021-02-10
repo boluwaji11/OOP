57c8f3473bde093f5804349eb37b531961571dc8
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 13:26:20 -0600

    Removed display method

--- Student_program.py
+++ Student_program.py
@@ -6,15 +6,12 @@ student_classification = input("Enter student classification (F,S,Jr,Sr): ")
 def main():
     age = sc.Student(student_classification)
 
-    age.agecal()
-    age.regperiod()
-
     print()
-    print("The student is", age.displayage(), "years old.")
+    print("The student is", age.agecal(), "years old.")
     print()
     print(
         "The Registration period for this student classification:",
-        age.displayregperiod(),
+        age.regperiod(),
     )
 
 
