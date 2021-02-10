3d9210d0aeb040b1f6767f22915eb5c319f18d03
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 13:18:20 -0600

    User input added

--- Student_program.py
+++ Student_program.py
@@ -1,15 +1,21 @@
 import StudentClass as sc
 
+student_classification = input("Enter student classification (F,S,Jr,Sr): ")
+
 
 def main():
-    age = sc.Student()
+    age = sc.Student(student_classification)
 
     age.agecal()
     age.regperiod()
 
+    print()
     print("The student is", age.displayage(), "years old.")
     print()
-    print("The Registration period for each classification:", age.displayregperiod())
+    print(
+        "The Registration period for this student classification:",
+        age.displayregperiod(),
+    )
 
 
 main()
