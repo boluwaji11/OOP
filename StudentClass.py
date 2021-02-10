bf2a8c03dbd2b0062ccda194e1d6e00d89b8a00a
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 13:18:08 -0600

    Edited with Decisions

--- StudentClass.py
+++ StudentClass.py
@@ -2,11 +2,11 @@ import datetime
 
 
 class Student:
-    def __init__(self):
+    def __init__(self, classification):
+        self.classification = classification
         self.studentid = "1234"
         self.name = "Bolu"
         self.dob = "04/11/2004"
-        self.classification = "F, S, Jr, Sr"
         self.age = 0
         self.period = 0
 
@@ -18,7 +18,16 @@ class Student:
         self.age = datetime.date.today().year - student_year
 
     def regperiod(self):
-        self.period = "\nSeniors – 11/1 thru 11/3 \nJuniors – 11/4 thru 11/6 \nSophomores – 11/7 thru 11/9 \nFreshmen – 11/10 thru 11/12"
+        if self.classification == "F":
+            self.period = "Freshmen - 11/10 thru 11/12"
+        elif self.classification == "S":
+            self.period = "Sophomores – 11/7 thru 11/9"
+        elif self.classification == "Jr":
+            self.period = "Juniors – 11/4 thru 11/6"
+        elif self.classification == "Sr":
+            self.period = "Seniors – 11/1 thru 11/3"
+        else:
+            self.period = "Invalid Classification"
 
     def displayage(self):
         return self.age
