da678a8c40fc68732bedf725e7368f28c32c2610
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 13:27:36 -0600

    Attributes set to private

--- StudentClass.py
+++ StudentClass.py
@@ -3,30 +3,30 @@ import datetime
 
 class Student:
     def __init__(self, classification):
-        self.classification = classification
-        self.studentid = "1234"
-        self.name = "Bolu"
-        self.dob = "04/11/2004"
-        self.age = 0
-        self.period = 0
+        self.__classification = classification
+        self.__studentid = "1234"
+        self.__name = "Bolu"
+        self.__dob = "04/11/2004"
+        self.__age = 0
+        self.__period = 0
 
     def agecal(self):
         # Extract year from DOB and convert to an integer
-        student_year = int(self.dob[6:])
+        student_year = int(self.__dob[6:])
 
         # Calculate the age
-        self.age = datetime.date.today().year - student_year
-        return self.age
+        self.__age = datetime.date.today().year - student_year
+        return self.__age
 
     def regperiod(self):
-        if self.classification == "F":
-            self.period = "Freshmen - 11/10 thru 11/12"
-        elif self.classification == "S":
-            self.period = "Sophomores – 11/7 thru 11/9"
-        elif self.classification == "Jr":
-            self.period = "Juniors – 11/4 thru 11/6"
-        elif self.classification == "Sr":
-            self.period = "Seniors – 11/1 thru 11/3"
+        if self.__classification == "F":
+            self.__period = "Freshmen - 11/10 thru 11/12"
+        elif self.__classification == "S":
+            self.__period = "Sophomores – 11/7 thru 11/9"
+        elif self.__classification == "Jr":
+            self.__period = "Juniors – 11/4 thru 11/6"
+        elif self.__classification == "Sr":
+            self.__period = "Seniors – 11/1 thru 11/3"
         else:
-            self.period = "Invalid Classification"
-        return self.period
+            self.__period = "Invalid Classification"
+        return self.__period
