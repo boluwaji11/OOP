a598d6a5d2cb2aa62154f6f05dfb22e13c88804b
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 13:26:02 -0600

    Removed display method

--- Insect Flight.py
+++ Insect Flight.py
@@ -5,11 +5,8 @@ def main():
     # Create an instance of the class
     flight = i.Insect()
 
-    # Run the random length
-    flight.flightlength()
-
     # Display the output
-    print("The length of flight is:", flight.display(), "miles.")
+    print("The length of flight is:", flight.flightlength(), "miles.")
 
 
 # Call the main function
