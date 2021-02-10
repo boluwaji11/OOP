0324c1da0008cedb44ed8b8a030b4b1f4a2253bd
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-10 14:10:59 -0600

    Request Arguments from users

--- Student_program.py
+++ Student_program.py
@@ -2,9 +2,13 @@ import StudentClass as sc
 
 
 def main():
+
+    studentID = input("Enter student ID: ")
+    name = input("Enter name: ")
+    dob = input("Enter dob in DD/MM/YYYY format: ")
     student_classification = input("Enter student classification (F,S,Jr,Sr): ")
 
-    age = sc.Student(student_classification)
+    age = sc.Student(studentID, name, dob, student_classification)
 
     print()
     print("The student is", age.agecal(), "years old.")
